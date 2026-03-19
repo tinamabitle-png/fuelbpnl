103b983b0e7e3aaf28f5236a33656f59bddcb4c1
apple <apple@apples-MacBook-Pro.local>
2026-03-20 01:11:49 +0200

    Use mnmnb gif on welcome banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,8 +63,8 @@
 
     <div class="glass rounded-2xl p-3 md:p-4 mt-8 overflow-hidden welcome-tween-card">
         <img
-            src="{{ asset('images/bmuntu.gif') }}"
-            alt="Bmuntu animated preview"
+            src="{{ asset('images/mnmnb.gif') }}"
+            alt="Mnmnb animated preview"
             class="welcome-tween-image is-in rounded-2xl"
             loading="lazy"
             data-welcome-tween="slide-in"
