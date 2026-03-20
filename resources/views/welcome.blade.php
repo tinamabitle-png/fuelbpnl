14e64f3eb8945ad207d504a5f71256545cbd5566
apple <apple@apples-MacBook-Pro.local>
2026-03-20 18:36:58 +0200

    Use wiseb gif for welcome preview and social image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Bwiser Control Platform')
 @section('meta_description', 'Bwiser is a South African fuel finance and payments platform for drivers, stations, vouchers, and settlements.')
 @section('canonical', url('/'))
-@section('og_image', asset('images/bw.gif'))
+@section('og_image', asset('images/wiseb.gif'))
 
 @section('content')
 <section class="max-w-7xl mx-auto px-6 pt-16 pb-20">
@@ -61,7 +61,7 @@
 
     <div class="glass rounded-2xl p-3 md:p-4 mt-8 overflow-hidden welcome-tween-card">
         <img
-            src="{{ asset('images/bw.gif') }}"
+            src="{{ asset('images/wiseb.gif') }}"
             alt="Bwiser animated preview"
             class="welcome-tween-image is-in rounded-2xl"
             loading="lazy"
@@ -310,7 +310,7 @@
         min-height: 420px;
         width: 100%;
         max-width: 380px;
-        justify-self: end;
+        justify-self: start;
     }
 
     .welcome-driver-stack {
