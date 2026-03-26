a960a9adabe7d9de718474b3a05780cd734c9e1d
apple <apple@apples-MacBook-Pro.local>
2026-03-26 19:43:24 +0200

    Welcome: dashboard CTA icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,8 +75,8 @@
                     @auth
                         @if($dashboardUrl)
                             <a class="bw-dashboard-button" href="{{ $dashboardUrl }}">
-                                <svg class="bw-dashboard-bell" viewBox="0 0 448 512" aria-hidden="true" focusable="false">
-                                    <path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"></path>
+                                <svg class="bw-dashboard-icon" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                    <path d="M3 13.5h8V3H3v10.5zM13 21h8V10.5h-8V21zM3 21h8v-6H3v6zM13 3v6h8V3h-8z"></path>
                                 </svg>
                                 <span class="bw-dashboard-label">Go to Dashboard</span>
                                 <span class="bw-dashboard-arrow" aria-hidden="true">›</span>
@@ -567,14 +567,12 @@
         outline-offset: 2px;
     }
 
-    .bw-dashboard-bell {
-        width: 14px;
-        height: 14px;
-        flex: 0 0 14px;
-    }
-
-    .bw-dashboard-bell path {
-        fill: rgb(0, 206, 62);
+    .bw-dashboard-icon {
+        width: 16px;
+        height: 16px;
+        flex: 0 0 16px;
+        color: rgb(96, 165, 250);
+        fill: currentColor;
     }
 
     .bw-dashboard-label {
