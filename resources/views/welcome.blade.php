3d3bf12c28cd83603338ca468c5670a9c67f505d
apple <apple@apples-MacBook-Pro.local>
2026-04-07 21:27:45 +0200

    Replace man hero with bench sign

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -277,7 +277,7 @@
 
         <div class="glass rounded-2xl p-3 md:p-4 overflow-hidden">
             <img
-                src="{{ asset('images/man.jpg') }}"
+                src="{{ asset('images/bench_sign.png') }}"
                 alt="Bwiser preview"
                 class="block w-full h-full object-cover rounded-2xl"
                 loading="lazy"
