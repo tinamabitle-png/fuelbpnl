123827b8ad3623fdad7036990556ee185b40ee25
apple <apple@apples-MacBook-Pro.local>
2026-05-16 15:57:16 +0200

    Fix Bwiser.gif reference

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -427,7 +427,7 @@
 
             <div class="bwiser-gif-card rounded-2xl overflow-hidden p-0" data-bwiser-gif-card>
                 <img
-                    src="{{ asset('images/Bwiser.gif') }}"
+src="{{ asset('images/Bwisergif') }}"
                     alt="Bwiser renewable energy vouchers and leasing preview"
                     class="bwiser-gif-card__bg"
                     data-welcome-media-target
